adcb75da9699db8a243a48f9c4d4df5d411822a0
Souvik Hui <155455536+SouvikHui@users.noreply.github.com>
2025-05-13 20:28:55 +0530

    Update models.py
    Related changes due to 'Helper' method in rag_qa.py

--- End-to-end RAG App/backend/models.py
+++ End-to-end RAG App/backend/models.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List,Optional
 
 class URLRequest(BaseModel):
     urls: List[str]
 
 class QueryRequest(BaseModel):
     question: str
-
+    chat_history: Optional[List[str]] = []
+    
 class QueryResponse(BaseModel):
     answer: str
