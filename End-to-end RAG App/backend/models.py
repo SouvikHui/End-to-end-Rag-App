0b38611772591b0fbdc3822c74ea409d716847db
Souvik Hui <155455536+SouvikHui@users.noreply.github.com>
2025-05-26 21:49:54 +0530

    Update models.py

--- End-to-end RAG App/backend/models.py
+++ End-to-end RAG App/backend/models.py
@@ -6,7 +6,10 @@ class URLRequest(BaseModel):
 
 class QueryRequest(BaseModel):
     question: str
-    chat_history: Optional[List[str]] = []
-    
+    chat_history: Optional[List[str]]
+
+class YT_URL(BaseModel):
+    yt_url: str
+ 
 class QueryResponse(BaseModel):
     answer: str
